7339956e5892c03cf33fe896b9ace80cd78ab173
Furzmund <20493918+Furzmund@users.noreply.github.com>
2024-09-27 14:12:46 -0600

    Feature - Project files

---
+++ src/main/java/org/furzmund/lightmeup/LightMeUpClient.java
@@ -0,0 +1,10 @@
+package org.furzmund.lightmeup;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LightMeUpClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/org/furzmund/lightmeup/LightMeUpDataGenerator.java
@@ -0,0 +1,11 @@
+package org.furzmund.lightmeup;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LightMeUpDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
